becac81dca633bf5e14982af35ee5034efbe4ccf
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-04-18 08:29:37 +0100

    Update model - add deadline, add validation of assignee

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,12 @@ class Todo < ActiveRecord::Base
   enumerize :status, in: [:new, :started, :done]
   belongs_to :requester, class_name: User
   belongs_to :assignee, class_name: User
+
+  validates :requester_id, presence: true
+
+  self.per_page = 10
+
+  def deadline_exceeded?
+    deadline < Date.today unless deadline.nil?
+  end
 end
